3d605d658db14f8dff1754d2c7379db151d697b0
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-11-01 08:58:56 -0300

    Add files via upload

---
+++ assets/preload/stages/BG_OSWALD.lua
@@ -0,0 +1,12 @@
+function onCreate()
+    makeLuaSprite('bg','BG_OSWALD',-600)
+    addLuaSprite('bg')
+makeAnimatedLuaSprite('grain', 'grain', 0, 0);
+addAnimationByPrefix('grain', 'grain', 'grain', 24, true);  
+objectPlayAnimation('grain', 'grain', true)
+scaleLuaSprite('grain', 2.75, 2.75);
+addLuaSprite('grain', false);
+setObjectCamera('grain', 'other');
+
+	setProperty('defaultCamZoom', 0.8) 
+end
---
+++ assets/preload/stages/oswald.lua
@@ -0,0 +1,4 @@
+function onCreate()
+	makeLuaSprite('house','BG_OSWALD', 0, 600)
+	addLuaSprite('house',false)
+end
